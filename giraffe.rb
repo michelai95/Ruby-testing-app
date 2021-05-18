dadd50948892e83be3a9e9cfef06bf3bcfe60308
michela iacobucci <miacobucci17@gmail.com>
2021-05-17 21:57:40 -0700

    lesson one

---
+++ giraffe.rb
@@ -0,0 +1,34 @@
+# give it a container 
+
+#  how to create variables
+character_age = "35"
+character_name = "John"
+
+puts ("Mike + " + character_name)
+# have to surround with paranthesis 
+puts "is cool"
+# you can change the variables as you code 
+character_name = "Dave"
+print "Hello, world"
+puts "\"Giraffe\" Academy"
+
+# will capatlize your string
+puts character_name.upcase()
+# will lowercase your string
+puts character_name.downcase()
+# will remove whitespace 
+puts character_name.strip()
+# will count the characters in a string 
+puts character_name.length()
+# will return a boolean
+puts character_name.include? "D"
+# find index of string
+puts character_name[2]
+# how you can grab a range of characters 
+puts character_name[2,4]
+# will show index of the character you are looking for 
+puts character_name.index("a")
+
+
+name = "Michela Iacobucci"
+occupation = "Software Developer"
